3e859d6d0c242dc1ba565dff1fbd3acd3b038b68
Engin Öztürk <engin.ozturk@bilyoner.com>
2016-09-05 17:48:12 +0300

    <Add/> icin test yazildi.

--- js/components/add.spec.js
+++ js/components/add.spec.js
@@ -1,9 +1,22 @@
 import React from "react";
 import {expect} from "chai"
 import {mount,shallow} from "enzyme";
+import Add from "../components/add";
 
 describe("<Add />", () => {
-  it("Test", () => {
-    expect(true).to.equal(true);
-  })
-})
+  it("Should created without explosion", () => {
+    let wrapper = shallow(<Add />);
+    expect(wrapper.find('.game-add')).to.have.length(1);
+  });
+
+  it("Should have 2 inputs for the `title` and `type`", () => {
+    let wrapper = shallow(<Add />);
+    expect(wrapper.find('input')).to.have.length(2);
+  });
+
+  it("Should have a button", () => {
+    let wrapper = shallow(<Add />);
+    expect(wrapper.find('button')).to.have.length(1);
+  });
+
+});
